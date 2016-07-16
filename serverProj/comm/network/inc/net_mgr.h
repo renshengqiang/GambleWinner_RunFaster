1d2902120091bdefb84cea7cab47c22f8ec4d25c
wangzaibing <992427850@qq.com>
2016-07-16 17:06:44 +0800

    init server framework

---
+++ serverProj/comm/network/inc/net_mgr.h
@@ -0,0 +1,4 @@
+#ifndef NET_MGR_H_
+#define NET_MGR_H_
+
+#endif //NET_MGR_H_
---
+++ serverProj/mainsvr/src/server_main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "net_mgr.h"
+
+int main() {
+	std::cout << "main server start ok!" << std::endl;
+}
